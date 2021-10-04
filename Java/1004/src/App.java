a3448fcd0ad5b84ba261bfa96e0aa937c0fff0d4
Yuuki915 <aso091335@gmail.com>
2021-10-04 11:59:12 -0700

    initial

---
+++ Java/1004/src/App.java
@@ -0,0 +1,23 @@
+public class App {
+    public static void main(String[] args) {
+        // System.out.println("Yatta!");
+        int value1 = 1;
+        int value2 = value1;
+        System.out.println(value1);
+        System.out.println(value2);
+
+        double num1 = 10.0d / 6.0d;
+        System.out.println(num1);
+
+        boolean isTrue = true;
+        System.out.println(isTrue);
+
+        String greeting = "Hello World";
+        System.out.println(greeting);
+
+        // type casting
+        int x = 1;
+        short y = (short)x;
+        System.out.println(y);
+    }
+}
