565db72c3bb9b26b594045eb8faacdd710f771c5
Yuuki915 <aso091335@gmail.com>
2021-10-06 12:25:08 -0700

    added 1006

---
+++ Java/1006/src/java01.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class java01 {
+
+    public static void main(String[] args) {
+        String string = new String("Hello");
+        System.out.println(string);
+
+        int x = 7;
+    }
+}
