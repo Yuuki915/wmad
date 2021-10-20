ccd417495b25c920fed9f68089a4c52b9106d03a
Yuki Hori <yuyu@Yukis-MacBook-Pro.local>
2021-10-20 13:25:00 -0700

    modi 1020

--- Java/1020/src/Lab3Q1/Q1.java
+++ Java/1020/src/Lab3Q1/Q1.java
@@ -7,10 +7,8 @@ import java.util.Scanner;
 
 public class Q1 {
     public static void main(String[] args) {
-
         
-        
-        // ArrayList<Integer> arrayList = new ArrayList<>();
+        ArrayList<Integer> arrayList = new ArrayList<>();
 
         Random random = new Random();
         int val = random.nextInt(50);
