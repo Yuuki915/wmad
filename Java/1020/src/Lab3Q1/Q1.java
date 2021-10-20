8880946bff98594f382d70c5ae1fb441ce42f2f0
Yuki Hori <yuyu@Yukis-MacBook-Pro.local>
2021-10-20 12:30:43 -0700

    modi 1020 file

--- Java/1020/src/Lab3Q1/Q1.java
+++ Java/1020/src/Lab3Q1/Q1.java
@@ -4,16 +4,22 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
+
 public class Q1 {
     public static void main(String[] args) {
 
-        ArrayList<Integer> arrayList = new ArrayList<>();
+        
+        
+        // ArrayList<Integer> arrayList = new ArrayList<>();
 
-        int[] array = new int[50];
-        for (int i = 1; i < 10; i++) {
-            array[i] = 1 + (int) (Math.random() * 50);
-            System.out.println(array[i]);
-        }
+        Random random = new Random();
+        int val = random.nextInt(50);
+        System.out.println(val);
+
+        // Scanner 
+
+
+        
 
     }
 }
