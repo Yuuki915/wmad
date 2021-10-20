b8eab6f1fe08464c9b53795a5bb47bc6bf666560
Yuki Hori <yuyu@Yukis-MacBook-Pro.local>
2021-10-20 13:58:44 -0700

    modi 1020

--- Java/1020/src/Lab3Q1/Q1.java
+++ Java/1020/src/Lab3Q1/Q1.java
@@ -8,13 +8,25 @@ import java.util.Scanner;
 public class Q1 {
     public static void main(String[] args) {
         
-        ArrayList<Integer> arrayList = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
 
-        Random random = new Random();
-        int val = random.nextInt(50);
-        System.out.println(val);
+        Random rnd = new Random();
+        for(int i=0; i<10; i++) {
+            list.add(rnd.nextInt(50) + 1);
+        }
 
-        // Scanner 
+        System.out.println(list);
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Put number: " + input.nextLine());
+
+        // int val = ;
+
+        // if(val == ) {
+        //    System.out.println("Exist im the list"); 
+        // } else {
+        //     System.out.println("Not exsist in the list");
+        // }
 
 
         
