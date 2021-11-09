768916a7d5bcfadff29a85bbbca02447131a7d75
Yuuki915 <aso091335@gmail.com>
2021-11-09 14:19:55 -0800

    add 1109pma file

---
+++ Java/sprong1109/src/main/java/com/example/sprong1109/Sprong1109Application.java
@@ -0,0 +1,13 @@
+package com.example.sprong1109;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprong1109Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprong1109Application.class, args);
+	}
+
+}
