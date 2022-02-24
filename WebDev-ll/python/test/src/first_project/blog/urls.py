e7691f84cf104d60d810a47d53fd4aa74b12df0f
Yuuki915 <aso091335@gmail.com>
2022-02-24 15:15:36 -0800

    add

--- WebDev-ll/python/test/src/first_project/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'blog'
-
-urlpatterns = [
-    path('', views.home, name='homepage'),
-    path('<str:pk>', views.single_post, name='single_post')
-]
