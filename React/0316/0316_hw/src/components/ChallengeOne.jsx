1c06666b1b87343195ba7263974b40f6637e81b2
Yuuki915 <aso091335@gmail.com>
2022-03-16 14:33:31 -0700

    add 0316hw

---
+++ React/0316/0316_hw/src/components/ChallengeOne.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "../App.css";
+import lookLeft from "../assets/look-left.jpeg";
+import lookRight from "../assets/look-right.jpeg";
+
+export default function ChallengeOne() {}
