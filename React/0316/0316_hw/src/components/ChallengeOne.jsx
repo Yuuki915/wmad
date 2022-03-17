2ab7956856262d6f4d5aea0e89a05b3759d77560
Yuuki915 <aso091335@gmail.com>
2022-03-17 02:01:59 -0700

    hw done

--- React/0316/0316_hw/src/components/ChallengeOne.jsx
+++ React/0316/0316_hw/src/components/ChallengeOne.jsx
@@ -1,6 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 import lookLeft from "../assets/look-left.jpeg";
 import lookRight from "../assets/look-right.jpeg";
 
-export default function ChallengeOne() {}
+export default function ChallengeOne() {
+  const [imgSrc, setImgSrc] = useState(lookLeft);
+
+  const toLeft = () => {
+    setImgSrc(lookLeft);
+  };
+
+  const toRight = () => {
+    setImgSrc(lookRight);
+  };
+
+  return (
+    <div className="one">
+      <h2>Challenge 1</h2>
+      <div className="img-container">
+        <img src={imgSrc} alt="" />
+      </div>
+      <div className="btns">
+        <button className="btn to-left" onClick={toLeft}>
+          ←
+        </button>
+        <button className="btn to-right" onClick={toRight}>
+          →
+        </button>
+      </div>
+    </div>
+  );
+}
