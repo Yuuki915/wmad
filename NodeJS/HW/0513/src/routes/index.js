9d81beeb613afdf3083d1357d525c24686a6ff4f
Yuuki915 <aso091335@gmail.com>
2022-05-14 03:12:43 -0700

    0513hw

--- NodeJS/HW/0513/src/routes/index.js
+++ NodeJS/HW/0513/src/routes/index.js
@@ -5,11 +5,11 @@ router.get("/", (req, res) => {
   const sql = "SELECT * FROM Todos ORDER BY Title";
   db.all(sql, [], (err, rows) => {
     if (err) return console.error(err.message);
-
     res.render("index", { model: rows });
   });
 });
 
+// --- create ---
 router.get("/create", (req, res) => {
   res.render("create", { model: {} });
 });
@@ -21,6 +21,38 @@ router.post("/create", (req, res) => {
   db.run(sql, todo, (err) => {
     if (err) return console.error(err.message);
 
+    res.redirect("/");
+  });
+});
+
+// --- edit ---
+router.get("/edit/:id", (req, res) => {
+  const sql = "SELECT * FROM Todos WHERE ID = ?";
+  const id = req.params.id;
+  db.get(sql, id, (err, rows) => {
+    if (err) return console.error(err.message);
+    res.render("edit", { model: { rows } });
+  });
+});
+
+router.post("/edit/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = `UPDATE Todos SET Title = ? WHERE ID = ${id}`;
+  const todo = [req.body.Title];
+
+  db.run(sql, todo, (err) => {
+    if (err) return console.error(err.message);
+
+    res.redirect("/");
+  });
+});
+
+// --- delete ---
+router.get("/delete/:id", (req, res) => {
+  const sql = `DELETE FROM Todos WHERE ID = ?`;
+  const id = req.params.id;
+  db.run(sql, id, (err) => {
+    if (err) return console.error(err.message);
     res.redirect("/");
   });
 });
--- NodeJS/W1/0513/src/server.js
+++ NodeJS/W1/0513/src/server.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 const app = express();
 
-app.set("view engine", "ejs");
+// app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "templates"));
 app.use(bodyParser.urlencoded({ extended: false }));
 
