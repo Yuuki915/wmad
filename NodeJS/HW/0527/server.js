03bd0d476233343c4eb37daf3ba476d6eb02e9ae
Yuuki915 <aso091335@gmail.com>
2022-05-24 23:43:54 -0700

    mp

--- NodeJS/HW/0527/server.js
+++ NodeJS/HW/0527/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 const blogRouter = require("./routes/blogs");
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017/0527-mid-proj");
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   const blogs = [
