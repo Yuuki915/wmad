960f008a9b9fa975b5dc61a22d9106f1d45014c2
Yuuki915 <aso091335@gmail.com>
2022-05-25 14:58:34 -0700

    mp

--- NodeJS/HW/0527/routes/blogs.js
+++ NodeJS/HW/0527/routes/blogs.js
@@ -6,12 +6,17 @@ router.get("/new", (req, res) => {
   res.render("newBlog");
 });
 
-router.get("/:id", (req, res) => {
-  res.send(req.params.id);
+router.get("/:slug", async (req, res) => {
+  let blog = await Blog.findOne({ slug: req.params.slug });
+
+  if (blog) {
+    res.render("show", { blog: blog });
+  } else {
+    res.redirect("/");
+  }
 });
 
 router.post("/", async (req, res) => {
-  //   console.log(req.body);
   let blog = new Blog({
     title: req.body.title,
     body: req.body.body,
@@ -20,10 +25,35 @@ router.post("/", async (req, res) => {
   try {
     blog = await blog.save();
 
-    res.redirect(`blogs/${blog.id}`);
+    res.redirect(`blogs/${blog.slug}`);
   } catch (error) {
     console.log(error);
   }
+});
+
+router.get("/edit/:id", async (req, res) => {
+  let blog = await Blog.findById(req.params.id);
+  res.render("edit", { blog: blog });
+});
+
+router.put("/:id", async (req, res) => {
+  req.blog = await Blog.findById(req.params.id);
+  let blog = req.blog;
+  blog.title = req.body.title;
+  blog.body = req.body.body;
+
+  try {
+    blog = await blog.save();
+    res.redirect(`/blogs/${blog.slug}`);
+  } catch (error) {
+    console.log(error);
+    res.redirect(`blog/edit/${blog.id}`, { blog: blog });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  await Blog.findByIdAndDelete(req.params.id);
+  res.redirect("/");
 });
 
 module.exports = router;
